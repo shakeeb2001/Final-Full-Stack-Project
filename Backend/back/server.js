200d339aca32b2a7af96eced9658a2888f439ad6
shakeeb2001 <ahamedshakeeb123@gmail.com>
2024-01-01 18:25:34 +0530

    final-commit

--- Backend/back/server.js
+++ Backend/back/server.js
@@ -8,21 +8,12 @@ const EventModel = require('../back/models/eventcardmodel');
 const DiningModel = require('../back/models/diningcardmodel');
 const BookingModel = require('../back/models/bookinghistrotymodel');
 
-const app = express();
-
-const corsOptions = 
-{
-    origin: ['https://final-full-stack-project-frontend.vercel.app'],
-    methods: ['POST', 'GET'],
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-  
-app.use(cors(corsOptions));
+const app = express(); 
+app.use(cors());
   
 app.use(express.json({ limit: '20mb' }));
 const uri = 'mongodb+srv://shakeeb:226284@mycluster.hitx68p.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri);
 
 const connection = mongoose.connection;
 const storage = multer.memoryStorage();
